6ffa9bbb4987ab6892819f932c54abd6d70b7ae2
DevJoshi030 <47146845+DevJoshi030@users.noreply.github.com>
2019-10-15 14:29:39 +0530

    Allowed hosts

--- devmail/devmail/settings.py
+++ devmail/devmail/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mtu!fvuio*m6$hz!mm^h!ykqzuoc0xlsebdht*(6@up$h6^dhi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
